f41f874fb863b918f50ce12cf2da54d882178a70
siwaaram <126309306+siwaaram@users.noreply.github.com>
2023-03-11 11:10:40 +0700

    การบ้าน(อีกแล้ว)

---
+++ reduce.py
@@ -0,0 +1,14 @@
+# n = 10
+# #  count up by 1
+# for i in range (0, n, 2):
+#     print(i)
+
+
+#  count down by 1
+# n = 10
+# for i in range (n, 0, -2):
+#     print(i)
+
+n = -10
+for i in range (n, 0, +1):
+    print(i)
