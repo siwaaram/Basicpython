af71a94d3e3c4755987553e1970f248068f63ccf
siwaaram <126309306+siwaaram@users.noreply.github.com>
2023-03-11 09:39:23 +0700

    การบ้านนับตัวอักษรกับพยัญชนะ

---
+++ main.py
@@ -0,0 +1,120 @@
+#name = "Peeravee"
+#last = "Arampongsanuvat"
+#age =15
+#brith ="30/08/2550"
+#high =165
+#weight =70
+#print(name)
+#print(last)
+#print(age)
+#print(brith)
+
+#print(weight)
+#fname = input("Input your first name ")
+#lname = input("Input your last name ")
+#age = int(input("Input your age "))
+#birth = input("Input your birth ")
+#high = float(input("Input your height "))
+#weight = float(input("input your weight"))
+#print('Hello '+fname + '' + lname + '' + '\n' + str(age) + '' + '\n'+birth + '' + '\n' + str(high) + '' + '\n' +
+     # str(weight))
+#import datetime
+#now = datetime.datetime.now()
+#print(now.strftime("%d/%m/@Y,%H:%M:%S"))
+#print("Current date and time:\n"+(now.strftime("%Y-%m-%d %H:%M:%S")))
+
+#datatime_str = '23/01/25'
+
+# import datetime
+# fname = input('Input your First name ')
+# lname = input('Input your last name ')
+# age = input('Input your age ')
+# weight = input('Input your weight ')
+# high = input('Input your height ')
+# datetime_str = input('Input your birth ')
+# datetime_object = datetime.datetime.strptime(datetime_str, '%y/%m/%d')
+# print(datetime_object)
+# print(type(datetime_object))
+# birth = datetime_object.strftime('%d/%m/%Y')
+# print("Hello "+fname + "" + "\t" + lname + "" + "\n" + str(age) + "" +"\n"+birth + "" + "\n" + str(high) + "" + "\n" +
+#       str(weight))
+# print(fname, type(fname) , "\t"+lname, type(lname),'\n', age,type(age),"\n",birth,type(birth),"\n",weight,type(weight),"\n"
+# ,high,type(high))
+
+
+
+#age = 15
+#print(age)
+#print(type(age))
+# import datetime
+# fname = "Peeravee"
+# lname = "Arampongsanuvat"
+# age = 15
+# birth = "50/08/30"
+# birth = datetime.datetime.strptime(birth, '%y/%m/%d')
+# weight = 70
+# high = 164.9
+# print(fname, type(fname) , "\t"+lname, type(lname),'\n', age,type(age),"\n",birth,type(birth),"\n",weight,type(weight),"\n"
+# ,high,type(high))
+# print("เครื่องคิดเลข101")
+# x = int(input("ใส่ตัวเลขตัวที่1 "  ))
+# y = int(input("ใส่ตัวเลทตัวที่2 "  ))
+# ans = x+y
+# print("ตัวเลขตัวที่1+ตัวเลขตัวที่2 เท่ากับ",ans)
+# ans = x-y
+# print("ตัวเลขตัวที่1-ตัวเลขตัวที่2 เท่ากับ",ans)
+# ans = x*y
+# print("ตัวเลขตัวที่1*ตัวเลขตัวที่2 เท่ากับ",ans)
+# ans = x/y
+# print("ตัวเลขตัวที่1/ตัวเลขตัวที่2 เท่ากับ",ans)
+# ans = x%y
+# print("ตัวเลขตัวที่1%ตัวเลขตัวที่2 เท่ากับ",ans)
+# ans = x**y
+# print("ตัวเลขตัวที่1**ตัวเลขตัวที่2 เท่ากับ",ans)
+# print("หาพื้นที่ของวงกลม")
+# r = int(input("ใส่รัศมีของวงกลม "))
+# ans = 22/7*r*r
+# print("พื้นที่ของวงกลมเท่ากับ ",ans)
+# from math import pi
+# r = float(input ("Input the radius of the circle : "))
+# area = pi * r**2
+# print ("The area of the circle with radius " + str(r) + " is: " + str(area))
+
+# age = int(input("Input your age: "))
+#
+# if age > 90:
+#     print("you are too old too party, granny")
+# elif age >= 18:
+#     print("you are allowed to party")
+# elif age >0:
+#     print("you are too young")
+# else:
+#     print("you are not born yet")
+
+
+#  ถ้า 0 ให้แสดงคำว่า you are not born yet
+# age = int(input("Input your age: "))
+#
+# if age > 90:
+#     print("you are too old too party, granny")
+# elif age >= 18 and age <0 :
+#     print("you are allowed to party")
+# else:
+#     print
+
+# grade = float(input("Input your grade: "))
+# if grade >= 80:
+#     print("You got grade A")
+# elif grade >=70 and grade <80 :
+#     print("You got grade B")
+# elif grade >=60 and grade <70 :
+#     print("You got grade C")
+# elif grade >=50 and grade <60 :
+#     print("You got grade D")
+# else :
+#     print("You got grade F")
+# num = int(input("Input number "))
+# if num%2==0:
+#     print(num,"is Even ")
+# elif num%2!=0:
+#     print(num,"is Odd ")
